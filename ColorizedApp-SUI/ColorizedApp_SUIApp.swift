d5c8b2b8377c4c3f978dcf597788b8971aec2fea
Samir Dzhafari <samirjafari2001@icloud.com>
2023-12-17 22:52:08 +0300

    Initial Commit

---
+++ ColorizedApp-SUI/ColorizedApp_SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedApp_SUIApp.swift
+//  ColorizedApp-SUI
+//
+//  Created by Самир Джафари on 17.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedApp_SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorizedApp-SUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ColorizedApp-SUI
+//
+//  Created by Самир Джафари on 17.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
